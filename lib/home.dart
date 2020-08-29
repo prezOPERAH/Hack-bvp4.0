2c539038d00358acb17676b3cb8dfcd7f52d2d6f
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2020-08-29 13:46:00 +0530

    Add files via upload

--- lib/home.dart
+++ lib/home.dart
@@ -8,15 +8,64 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children= [
+    HomeView(),
+    ExplorePage(),
+    ProgressPage(),
+
+  ];
 
 
 
 
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("EDU PLANNER"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CourseView(course:newCourse,)),
+              );
+            },
+          )
+        ],
+      ),
+    body: _children[_currentIndex],
+    bottomNavigationBar: BottomNavigationBar(
+      onTap: onTabTapped,
+      currentIndex: _currentIndex,
+      items: [
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.home),
+          title: new Text("Home"),
+
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.explore),
+          title: new Text("Explore"),
+
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.history),
+          title: new Text("Progress"),
+
+        ),
+      ]
+      ),  
     );
   }
+}
+ 
+void onTabTapped( int index) {
+  setState(() {
+    _currentIndex= index;
+  }); 
+ }
 }
